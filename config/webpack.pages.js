53a1b51f51d56b7053c3e8e1483255d0041ca5eb
Anna Komkova <ann.komkova98@gmail.com>
2025-09-23 13:26:48 +0300

    upd links

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -11,7 +11,12 @@ const htmlPages = [
   createPages('./src/index.html', './index.html'),
   createPages('./src/pages/articles.html', './pages/articles.html'),
   createPages('./src/pages/tests.html', './pages/tests.html'),
-  createPages('./src/pages/dictionary.html', './pages/dictionary.html')
+  createPages('./src/pages/dictionary.html', './pages/dictionary.html'),
+  createPages(
+    './src/pages/articles/plants.html',
+    './pages/articles/plants.html'
+  ),
+  createPages('./src/pages/tests/test1.html', './pages/tests/test1.html')
 ]
 
 module.exports = htmlPages
