e3059d740e3a222458ebef404f56d32f7103ca64
Anna Komkova <ann.komkova98@gmail.com>
2026-01-30 14:46:43 +0300

    вывод результата теста

--- src/javascripts/tests.js
+++ src/javascripts/tests.js
@@ -20,13 +20,13 @@ function initTest(stages) {
   }
 }
 
-function chooseAnswer(stages) {
+function chooseAnswer(stages, results) {
   checkboxes.forEach((checkbox) => {
     checkbox.addEventListener('change', () => {
       if (checkbox.checked) {
         setTimeout(() => {
           resultCount += Number(checkbox.dataset.count)
-          updateStage(stages)
+          updateStage(stages, results)
           checkbox.checked = false
         }, 300)
       }
@@ -34,12 +34,77 @@ function chooseAnswer(stages) {
   })
 }
 
-function updateStage(stages) {
+function updateStage(stages, results) {
   if (++currentStage < stages.length) {
     initTest(stages)
   } else {
-    console.log('результат')
+    showResult(results)
   }
+}
+
+function showResult(results) {
+  const testContainer = document.querySelector('.O_Test')
+  testContainer.innerHTML = ''
+
+  const resultWrapper = document.createElement('div')
+  resultWrapper.classList.add('M_TestResult')
+
+  const resultCnt = document.createElement('p')
+  resultCnt.classList.add('A_TestResultCount')
+  resultCnt.innerText = `итого: ${resultCount}`
+
+  const resultHeader = document.createElement('h2')
+  resultHeader.classList.add('A_TestResultHeader')
+
+  const resultParagraph = document.createElement('p')
+  resultParagraph.classList.add('A_TestResultParagraph')
+
+  const resultImage = document.createElement('img')
+  resultImage.classList.add('A_TestResultImage')
+
+  if (resultCount == 4) {
+    resultHeader.innerText = results[0].header
+    resultParagraph.innerText = results[0].paragraph
+    resultImage.src = results[0].image
+  } else if (resultCount == 3 || resultCount == 2) {
+    resultHeader.innerText = results[1].header
+    resultParagraph.innerText = results[1].paragraph
+    resultImage.src = results[1].image
+  } else {
+    resultHeader.innerText = results[2].header
+    resultParagraph.innerText = results[2].paragraph
+    resultImage.src = results[2].image
+  }
+
+  // switch (resultCount) {
+  //   case 4:
+  //     resultHeader.innerText = results[0].header
+  //     resultParagraph.innerText = results[0].paragraph
+  //     resultImage.src = results[0].image
+  //     break
+  //   case 3:
+  //     resultHeader.innerText = results[1].header
+  //     resultParagraph.innerText = results[1].paragraph
+  //     resultImage.src = results[1].image
+  //     break
+  //   case 2:
+  //     resultHeader.innerText = results[1].header
+  //     resultParagraph.innerText = results[1].paragraph
+  //     resultImage.src = results[1].image
+  //     break
+  //   default:
+  //     resultHeader.innerText = results[2].header
+  //     resultParagraph.innerText = results[2].paragraph
+  //     resultImage.src = results[2].image
+  //     break
+  // }
+
+  resultWrapper.appendChild(resultCnt)
+  resultWrapper.appendChild(resultHeader)
+  resultWrapper.appendChild(resultParagraph)
+  resultWrapper.appendChild(resultImage)
+
+  testContainer.appendChild(resultWrapper)
 }
 
 export { initTest, chooseAnswer }
