80c7926d03712a2c4e04b757888d1ead622268ce
Anna Komkova <ann.komkova98@gmail.com>
2026-02-03 12:42:49 +0300

    темная тема на cookies

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,2 +1,32 @@
 console.log('hey')
 import '../stylesheets/index.css'
+
+import Cookies from 'js-cookie'
+
+initSwitch()
+switchTheme()
+
+function initSwitch() {
+  const toggleSwitch = document.querySelector('.Q_SwitchCheckbox')
+  toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+      Cookies.set('theme', 'dark')
+    } else {
+      Cookies.remove('theme')
+    }
+
+    switchTheme()
+  })
+}
+
+function switchTheme() {
+  const body = document.querySelector('body')
+  const toggleSwitch = document.querySelector('.Q_SwitchCheckbox')
+
+  if (Cookies.get('theme') == 'dark') {
+    body.classList.add('dark')
+    toggleSwitch.checked = true
+  } else {
+    body.classList.remove('dark')
+  }
+}
