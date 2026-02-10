8b3811c9f84cdd9c8b4dd8b92644bb5421706b66
Anna Komkova <ann.komkova98@gmail.com>
2026-02-10 13:05:58 +0300

    тизеры статей через airtable

--- src/javascripts/articles.js
+++ src/javascripts/articles.js
@@ -1,3 +1,4 @@
+// настройки подключения свайпера (галереи)
 const swiper = new Swiper('.swiper', {
   // Optional parameters
 
@@ -17,3 +18,93 @@ const swiper = new Swiper('.swiper', {
   //   prevEl: '.swiper-button-prev'
   // }
 })
+
+// подключение и работа с базой данных airtable
+
+import Airtable from 'airtable'
+
+const token =
+  'patVPdVyFbH20dWh6.758467647504d4e8a79f1c02906b3c3c5d68b62841f0603dcbf0b165bc2c7e52'
+
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: token
+})
+
+var base = Airtable.base('appW4mogQFemHeJy8')
+
+let data
+getArticlesTeasers().then((content) => {
+  data = content
+
+  updateInfo(data)
+})
+
+function getArticlesTeasers() {
+  return new Promise((resolve, reject) => {
+    const content = []
+
+    base('articles teasers')
+      .select({
+        maxRecords: 100
+      })
+      .firstPage()
+      .then((result) => {
+        result.forEach((record) => {
+          content.push({
+            id: record.id,
+            title: record.fields['Title'],
+            description: record.fields['Description'],
+            tags: record.fields['Tags'],
+            image: record.fields['Images'],
+            url: record.fields['URL']
+          })
+        })
+
+        resolve(content)
+      })
+  })
+}
+
+function updateInfo(content) {
+  content.forEach((stroke) => {
+    createArticleTeaserCard(stroke)
+  })
+}
+
+function createArticleTeaserCard(stroke) {
+  let { title, description, image, tags, url } = stroke
+
+  const card = document.createElement('a')
+  card.href = url
+  card.classList.add('W_ArticleTeaser')
+
+  const header = document.createElement('h3')
+  header.innerText = title
+  header.classList.add('A_ArticleTeaserHeader')
+
+  const text = document.createElement('p')
+  text.innerText = description
+  text.classList.add('A_ArticleTeaserDescription')
+
+  const articleTags = document.createElement('div')
+  articleTags.classList.add('C_ArticleTeaserTags')
+
+  // const articleImage = document.createElement('img')
+  // articleImage.src = image
+
+  tags.forEach((tag) => {
+    const articleTag = document.createElement('span')
+    articleTag.classList.add('A_ArticleTeaserTag')
+    articleTag.innerText = tag
+
+    articleTags.appendChild(articleTag)
+  })
+
+  card.appendChild(header)
+  card.appendChild(text)
+  card.appendChild(articleTags)
+  // card.appendChild(articleImage)
+
+  document.querySelector('.O_ArticleTeasers').appendChild(card)
+}
